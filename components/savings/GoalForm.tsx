732ebbb09c2a331c82fbfb55acd0e337d58fdd3c
Shecodes174 <mmesomagoodness202@gmail.com>
2026-08-26 10:21:19 +0000

    added

--- components/savings/GoalForm.tsx
+++ components/savings/GoalForm.tsx
@@ -121,6 +121,7 @@ export default function GoalForm({ open, onOpenChange, onGoalCreated }: GoalForm
                         <input
                             id="targetAmount"
                             type="number"
+                            inputMode="decimal"
                             {...register('targetAmount')}
                             className={`w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-green-500 outline-none transition-all ${errors.targetAmount ? 'border-red-500 bg-red-50' : 'border-gray-300 dark:border-gray-600 dark:bg-gray-700'
                                 }`}
